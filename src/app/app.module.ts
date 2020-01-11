8a6a47efea1b4fba0c64ee7a8804368cd81a1520
GeroCoded <luisfergeronimo@gmail.com>
2020-01-11 16:35:32 -0600

    2nd Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { reducers } from './app.reducers';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { TodosListComponent } from './todo/todos-list/todos-list.component';
+import { TodosItemComponent } from './todo/todos-item/todos-item.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { FooterComponent } from './footer/footer.component';
+
+// Config
+import { environment } from '../environments/environment';
+import { FilterPipe } from './filter/filter.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		FooterComponent,
+		TodoComponent,
+		TodosListComponent,
+		TodosItemComponent,
+		TodoFooterComponent,
+		TodoAddComponent,
+		FilterPipe
+	],
+	imports: [
+		BrowserModule,
+		ReactiveFormsModule,
+		StoreModule.forRoot( reducers ),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
